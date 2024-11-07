9c1148fb1ef3c7396446385e26980732543249de
Liav Sarfati <liavsarfati@h-MacBook-Pro-sl-Liav.local>
2024-11-07 15:53:53 +0200

    Final Check

--- FVCalc.java
+++ FVCalc.java
@@ -6,8 +6,6 @@ public class FVCalc {
 		double rate = Double.parseDouble(args[1]);
 		int years = Integer.parseInt(args[2]);
 
-		System.out.println(value + " " + rate + " " + years);
-
 		double newRate = (double)(rate);
 		double newYears = (double)(years);
 		double newValue = (double)(value);
