51764b34a9d5ba496cc4d60d6097ac829dfd3c0e
Liav Sarfati <liavsarfati@h-MacBook-Pro-sl-Liav.local>
2024-11-07 14:03:05 +0200

    liav HW

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,24 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		
+		int value = Integer.parseInt(args[0]);
+		int rate = Integer.parseInt(args[1]);
+		int years = Integer.parseInt(args[2]);
+
+		System.out.println(value + " " + rate + " " + years);
+
+		double newRate = (double)(rate);
+		double newYears = (double)(years);
+		double newValue = (double)(value);
+
+		newRate = Math.pow(((newRate / 100.0) + 1), newYears); // calculating new rate
+		newValue = newValue * newRate ; // calculating new value
+
+		System.out.println("after " + years + " years, $" + value + " saved at " 
+		+ (double) (rate) + " will yeild $" + (int) (newValue));
+	
+
+		
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,23 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    
+
+		int number = Integer.parseInt(args[0]);
+
+		number = Math.floorMod(number, 1000); // making sure we checking from the hunderds digit
+
+		int hundreds  = Math.floorDiv(number, 100); //extracting the hundreds
+		int tens = Math.floorDiv(Math.floorMod(number, 100), 10); // extracting the tens
+		int ones = Math.floorDiv(Math.floorMod(Math.floorMod(number, 100), 10), 1); // extracting the ones
+
+		System.out.println("There are : " + hundreds + " hundreds, " + tens + " tens, " + ones + " ones.");
+
+
+		
+		
+
+
+
 	}
 }
